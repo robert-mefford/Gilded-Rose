7bcfa07f472cb58dad185dfc3f065c06c59a623d
robert_mefford <robertmefford66@outlook.com>
2022-11-02 22:06:41 -0400

    js_jest update functionality when item name starts with `Conjured`

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -12,10 +12,11 @@ class Shop {
   }
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
+      let decreasement = (this.items[i].name.startsWith("Conjured") ? 2 : 1);
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
+            this.items[i].quality = this.items[i].quality - decreasement;
           }
         }
       } else {
@@ -43,7 +44,7 @@ class Shop {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
+                this.items[i].quality = this.items[i].quality - decreasement;
               }
             }
           } else {
