4ef6cecbc8fcb6941dd98ff9490371572e4bee51
robert_mefford <robertmefford66@outlook.com>
2022-11-02 22:52:35 -0400

    js_mocha update functionality and add test cases for all types of items

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -11,17 +11,18 @@ class Shop {
     this.items = items;
   }
   updateQuality() {
-    for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+    for (let i = 0; i < this.items.length; i++) {
+      let decreasement = (this.items[i].name.startsWith("Conjured") ? 2 : 1);
+      if (!this.items[i].name.startsWith('Aged Brie') && !this.items[i].name.startsWith('Backstage passes')) {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
+          if (!this.items[i].name.startsWith('Sulfuras')) {
+            this.items[i].quality = this.items[i].quality - decreasement;
           }
         }
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (this.items[i].name.startsWith('Backstage passes')) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
@@ -35,15 +36,15 @@ class Shop {
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+      if (!this.items[i].name.startsWith('Sulfuras')) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (!this.items[i].name.startsWith('Aged Brie')) {
+          if (!this.items[i].name.startsWith('Backstage passes')) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
+              if (!this.items[i].name.startsWith('Sulfuras')) {
+                this.items[i].quality = this.items[i].quality - decreasement;
               }
             }
           } else {
@@ -60,6 +61,7 @@ class Shop {
     return this.items;
   }
 }
+
 module.exports = {
   Item,
   Shop
--- js-mocha/test/test_gilded_rose.js
+++ js-mocha/test/test_gilded_rose.js
@@ -1,11 +1,111 @@
 var {expect} = require('chai');
-var {Shop, Item} = require('../src/gilded_rose.js');
-describe("Gilded Rose", function() {
+const {Shop, Item} = require("../src/gilded_rose");
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+describe("Normal Items", function() {
+  it("`quality` can't be negative", function() {
+    const gildedRose = new Shop([new Item("foo", 0, 0), new Item("foo", 2, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("fixme");
+    expect(items[0].quality).to.equal(0);
+    expect(items[1].quality).to.equal(0);
+    expect(items[0].sellIn).to.equal(-1);
+    expect(items[1].sellIn).to.equal(1);
   });
 
+  it("`quality` and `sellIn` decrease by 1", function() {
+    const gildedRose = new Shop([new Item("foo", 2, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(9);
+    expect(items[0].sellIn).to.equal(1);
+  });
+
+  it("`quality` decrease by 2 when `sellIn` is negative", function() {
+    const gildedRose = new Shop([new Item("foo", 0, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(8);
+    expect(items[0].sellIn).to.equal(-1);
+  })
+});
+
+describe("Aged Brie", function() {
+  it("`quality` increase by 1 when `sellIn` is positive", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 2, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(11);
+    expect(items[0].sellIn).to.equal(1);
+  });
+
+  it("`quality` increase by 2 when `sellIn` is negative", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 0, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(12);
+    expect(items[0].sellIn).to.equal(-1);
+  })
+});
+
+describe("Sulfuras", function() {
+  it("`quality` and `sellIn` never change", function() {
+    const gildedRose = new Shop([new Item("Sulfuras", 0, 80)]);
+    let items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(80);
+    expect(items[0].sellIn).to.equal(0);
+    items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(80);
+    expect(items[0].sellIn).to.equal(0);
+  });
+});
+
+describe("Backstage passes", function() {
+  it("`quality` increase by 1 if `sellIn` is over 10", function() {
+    const gildedRose = new Shop([new Item("Backstage passes", 12, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(11);
+    expect(items[0].sellIn).to.equal(11);
+  });
+
+  it("`quality` increase by 2 if `sellIn` is between 4 and 10", function() {
+    const gildedRose = new Shop([new Item("Backstage passes", 9, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(12);
+    expect(items[0].sellIn).to.equal(8);
+  });
+
+  it("`quality` increase by 3 if `sellIn` is between 0 and 5", function() {
+    const gildedRose = new Shop([new Item("Backstage passes", 5, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(13);
+    expect(items[0].sellIn).to.equal(4);
+  });
+
+  it("`quality` has to be 0 if `sellIn` is negative", function() {
+    const gildedRose = new Shop([new Item("Backstage passes", 0, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(0);
+    expect(items[0].sellIn).to.equal(-1);
+  });
+});
+
+describe("Conjured Items", function() {
+  it("`quality` can't be negative", function() {
+    const gildedRose = new Shop([new Item("Conjured", 0, 0), new Item("Conjured", 2, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(0);
+    expect(items[1].quality).to.equal(0);
+
+    expect(items[0].sellIn).to.equal(-1);
+    expect(items[1].sellIn).to.equal(1);
+  });
+
+  it("`quality` and `sellIn` decrease by 2", function() {
+    const gildedRose = new Shop([new Item("Conjured", 2, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(8);
+    expect(items[0].sellIn).to.equal(1);
+  });
+
+  it("`quality` decrease by 4 when `sellIn` is negative", function() {
+    const gildedRose = new Shop([new Item("Conjured", 0, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(6);
+    expect(items[0].sellIn).to.equal(-1);
+  })
 });
