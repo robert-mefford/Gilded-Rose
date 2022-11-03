67b7fbf2403779ea23ef9c7ea4552863b8eb3828
robert_mefford <robertmefford66@outlook.com>
2022-11-02 23:18:05 -0400

    TypeScript add mocha test cases for all types of itmes

--- TypeScript/test/mocha/gilded-rose.spec.ts
+++ TypeScript/test/mocha/gilded-rose.spec.ts
@@ -1,10 +1,111 @@
-import { expect } from 'chai';
+import { should } from 'chai';
 import { Item, GildedRose } from '@/gilded-rose';
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+describe("Normal Items", function() {
+  it("`quality` can't be negative", function() {
+    const gildedRose = new GildedRose([new Item("foo", 0, 0), new Item("foo", 2, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal('fixme');
+    should().equal(items[0].quality, 0);
+    should().equal(items[1].quality, 0);
+    should().equal(items[0].sellIn, -1);
+    should().equal(items[1].sellIn, 1);
   });
+
+  it("`quality` and `sellIn` decrease by 1", function() {
+    const gildedRose = new GildedRose([new Item("foo", 2, 10)]);
+    const items = gildedRose.updateQuality();
+    should().equal(items[0].quality, 9);
+    should().equal(items[0].sellIn, 1);
+  });
+
+  it("`quality` decrease by 2 when `sellIn` is negative", function() {
+    const gildedRose = new GildedRose([new Item("foo", 0, 10)]);
+    const items = gildedRose.updateQuality();
+    should().equal(items[0].quality, 8);
+    should().equal(items[0].sellIn, -1);
+  })
+});
+
+describe("Aged Brie", function() {
+  it("`quality` increase by 1 when `sellIn` is positive", function() {
+    const gildedRose = new GildedRose([new Item("Aged Brie", 2, 10)]);
+    const items = gildedRose.updateQuality();
+    should().equal(items[0].quality, 11);
+    should().equal(items[0].sellIn, 1);
+  });
+
+  it("`quality` increase by 2 when `sellIn` is negative", function() {
+    const gildedRose = new GildedRose([new Item("Aged Brie", 0, 10)]);
+    const items = gildedRose.updateQuality();
+    should().equal(items[0].quality, 12);
+    should().equal(items[0].sellIn, -1);
+  })
+});
+
+describe("Sulfuras", function() {
+  it("`quality` and `sellIn` never change", function() {
+    const gildedRose = new GildedRose([new Item("Sulfuras", 0, 80)]);
+    let items = gildedRose.updateQuality();
+    should().equal(items[0].quality, 80);
+    should().equal(items[0].sellIn, 0);
+    items = gildedRose.updateQuality();
+    should().equal(items[0].quality, 80);
+    should().equal(items[0].sellIn, 0);
+  });
+});
+
+describe("Backstage passes", function() {
+  it("`quality` increase by 1 if `sellIn` is over 10", function() {
+    const gildedRose = new GildedRose([new Item("Backstage passes", 12, 10)]);
+    const items = gildedRose.updateQuality();
+    should().equal(items[0].quality, 11);
+    should().equal(items[0].sellIn, 11);
+  });
+
+  it("`quality` increase by 2 if `sellIn` is between 4 and 10", function() {
+    const gildedRose = new GildedRose([new Item("Backstage passes", 9, 10)]);
+    const items = gildedRose.updateQuality();
+    should().equal(items[0].quality, 12);
+    should().equal(items[0].sellIn, 8);
+  });
+
+  it("`quality` increase by 3 if `sellIn` is between 0 and 5", function() {
+    const gildedRose = new GildedRose([new Item("Backstage passes", 5, 10)]);
+    const items = gildedRose.updateQuality();
+    should().equal(items[0].quality, 13);
+    should().equal(items[0].sellIn, 4);
+  });
+
+  it("`quality` has to be 0 if `sellIn` is negative", function() {
+    const gildedRose = new GildedRose([new Item("Backstage passes", 0, 10)]);
+    const items = gildedRose.updateQuality();
+    should().equal(items[0].quality, 0);
+    should().equal(items[0].sellIn, -1);
+  });
+});
+
+describe("Conjured Items", function() {
+  it("`quality` can't be negative", function() {
+    const gildedRose = new GildedRose([new Item("Conjured", 0, 0), new Item("Conjured", 2, 0)]);
+    const items = gildedRose.updateQuality();
+    should().equal(items[0].quality, 0);
+    should().equal(items[1].quality, 0);
+
+    should().equal(items[0].sellIn, -1);
+    should().equal(items[1].sellIn, 1);
+  });
+
+  it("`quality` and `sellIn` decrease by 2", function() {
+    const gildedRose = new GildedRose([new Item("Conjured", 2, 10)]);
+    const items = gildedRose.updateQuality();
+    should().equal(items[0].quality, 8);
+    should().equal(items[0].sellIn, 1);
+  });
+
+  it("`quality` decrease by 4 when `sellIn` is negative", function() {
+    const gildedRose = new GildedRose([new Item("Conjured", 0, 10)]);
+    const items = gildedRose.updateQuality();
+    should().equal(items[0].quality, 6);
+    should().equal(items[0].sellIn, -1);
+  })
 });
