68e490da00019f16a60e9d4aa6e39a68e3086199
robert_mefford <robertmefford66@outlook.com>
2022-11-02 22:16:57 -0400

    js_jest add test for normal items

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,26 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+describe("Normal Items", function() {
+  it("quality can't be negative", function() {
+    const gildedRose = new Shop([new Item("foo", 0, 0), new Item("foo", 2, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].quality).toBe(0);
+    expect(items[1].quality).toBe(0);
+    expect(items[0].sellIn).toBe(-1);
+    expect(items[1].sellIn).toBe(1);
   });
+
+  it("quality and sellIn decrease by 1", function() {
+    const gildedRose = new Shop([new Item("foo", 2, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(9);
+    expect(items[0].sellIn).toBe(1);
+  });
+
+  it("quality decrease by 2 when sellIn is negative", function() {
+    const gildedRose = new Shop([new Item("foo", 0, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(8);
+    expect(items[0].sellIn).toBe(-1);
+  })
 });
